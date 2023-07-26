e0cf3b9783e81d4a6693d7946a6a0e439ccc4f0c
PainKillerOFC <7lucasdaniel@gmail.com>
2023-07-26 18:49:15 -0300

    2023-07-26

--- app_home/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    id_user = models.AutoField(primary_key=True)
-    email = models.TextField(max_length=255)
-    password = models.TextField(max_length=255)
